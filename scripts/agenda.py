367cbe0bd602bcb7ba7335639ad0cf0706bc776f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-01-31 03:41:24 +0100

    script: parse agenda and print links

--- scripts/agenda.py
+++ scripts/agenda.py
@@ -10,6 +10,7 @@ Go to https://github.com/orgs/ITISFoundation/projects/9/views/1
 
 import csv
 import logging
+import re
 from pathlib import Path
 from typing import List
 
@@ -25,7 +26,7 @@ def to_md_row(row: List[str]):
 
 
 def to_md(csv_path: Path):
-    md_path = csv_path.with_suffix(".md") 
+    md_path = csv_path.with_suffix(".md")
 
     with csv_path.open() as csvfile:
         reader = csv.DictReader(csvfile)
@@ -33,7 +34,7 @@ def to_md(csv_path: Path):
         with md_path.open("wt") as md:
             print(to_md_row(COLUMNS), file=md)
             print(to_md_row(["--"] * len(COLUMNS)), file=md)
-            
+
             current_topic = None
             for row in reader:
                 # group
@@ -44,18 +45,50 @@ def to_md(csv_path: Path):
                     indented = True
                     title = f"<blockquote>{title}</blockquote>"
                 current_topic = topic
-                
 
                 # write
-                col_topic ="" if (indented or topic=="undefined") else topic
-                print(to_md_row([row["PO Priority"], , title, row["Assignees"], row["Status"], "", "" ]) , file=md)
+                col_topic = "" if (indented or topic == "undefined") else topic
+                print(
+                    to_md_row(
+                        [
+                            row["PO Priority"],
+                            col_topic,
+                            title,
+                            row["Assignees"],
+                            row["Status"],
+                            "",
+                            "",
+                        ]
+                    ),
+                    file=md,
+                )
 
     return md_path
 
 
-if __name__ == "__main__":
+def create_md_from_csv():
     for csv_path in Path.cwd().glob("*.csv"):
         logger.info("Processing %s ...", csv_path)
         md_path = to_md(csv_path)
         logger.info("Created %s", md_path)
 
+
+def parse_agenda_and_print_links(agenda_md):
+
+    issues = []
+    for issue_number in re.findall(r"\[#(\d+)\][^:]", Path(agenda_md).read_text()):
+        issues.append(int(issue_number))
+
+    issues = sorted(issues)
+    for issue_number in issues:
+        # heuristic way to determin the repo
+        repo_name = "osparc-issues" if issue_number < 2000 else "osparc-simcore"
+
+        print(
+            f"[#{issue_number}]: https://github.com/ITISFoundation/{repo_name}/issues/{issue_number}"
+        )
+
+
+if __name__ == "__main__":
+
+    parse_agenda_and_print_links("../reviews/20220128_Rudolph.md")
