bb28ece9f0d9315c503c53f42f633792fa70c4a5
Andrei Neagu <neagu@itis.swiss>
2024-07-17 10:41:21 +0200

    refactor tooling to assemble release drats for all products

--- scripts/make_release_drfts.py
+++ scripts/make_release_drfts.py
@@ -7,8 +7,7 @@ CURRENT_DIR: Path = Path(__file__).resolve().parent
 _TEMPLATE: str = """Click on the link for a list of all the PRs released since `{version_tag}` 
 https://github.com/ITISFoundation/osparc-simcore/compare/{version_tag}...master
 
-# Draft release notes go below the line
-
+**Draft release notes go below the line**
 ---
 """
 
@@ -23,6 +22,17 @@ def _validate_version(version):
     return version
 
 
+def _list_folders_in_path(path: Path) -> list[Path]:
+    return [x for x in path.glob("*") if x.is_dir()]
+
+
+def _get_previous_version(vtag: str) -> int:
+    version_parts = vtag.strip("v").split(".")
+    version_parts[1] = f"{int(version_parts[1])-1}"
+
+    return "v" + ".".join(version_parts)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Process a version number in the format X.X.X"
@@ -33,14 +43,16 @@ def main():
     args = parser.parse_args()
 
     tag = f"v{args.version}"
+
     print(f"Will generate form tag: {tag}")
 
-    new_draft_content = _TEMPLATE.format(version_tag=tag)
-    draft_file = CURRENT_DIR / ".." / "release-drafts" / f"{tag}.md"
-    if draft_file.exists():
-        msg = f"{draft_file} already exists, make sure you are creating the draft for the correct release"
-        raise ValueError(msg)
-    draft_file.write_text(new_draft_content)
+    new_draft_content = _TEMPLATE.format(version_tag=_get_previous_version(tag))
+    for product_folder in _list_folders_in_path(CURRENT_DIR / ".." / "release-notes"):
+        draft_file = product_folder / f"{tag}.md"
+        if draft_file.exists():
+            msg = f"{draft_file} already exists, make sure you are creating the draft for the correct release"
+            raise ValueError(msg)
+        draft_file.write_text(new_draft_content)
 
 
 if __name__ == "__main__":
